60c89ce0726f9962692cd1be0d786206f6e4414f
shyamsundar.ja <shyamsundar.ja@zohocorp.com>
2025-03-08 17:54:34 +0530

    Initial Commit

---
+++ Focus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Focus
+//
+//  Created by shyam-15059 on 08/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Focus/FocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusApp.swift
+//  Focus
+//
+//  Created by shyam-15059 on 08/03/25.
+//
+
+import SwiftUI
+
+@main
+struct FocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
